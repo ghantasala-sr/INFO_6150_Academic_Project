a310da2fddf532fc3e6cc018aca6fac1ffa8e67c
harshakallem22 <harshavardhanreddy.k@northeastern.edu>
2024-10-11 23:38:54 -0400

    done validation for login textfields

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const loginForm = document.getElementById('loginForm');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    const emailRegex = /^[a-z0-9]+@gmail\.com$/;
+    if (!emailRegex.test(email)) {
+        alert('Invalid email id. Please enter a valid email id.');
+        return;
+    }
+
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#\$%\^&\*\(\)_\+\-=\[\]{};':"\\|,.<>\/?])[A-Za-z\d!@#\$%\^&\*\(\)_\+\-=\[\]{};':"\\|,.<>\/?]*$/;
+
+    if (!passwordRegex.test(password)) {
+        alert('Invalid password. The password must contain atleast one uppercase letter, one lowercase letter, one number, one special character.');
+        return;
+    }
+
+    window.location.href = 'index.html';
+});
